f1aa9bc2542fd7b58c06edb4e54cb9417ca35008
Youn Mélois <youn@melois.dev>
2024-11-04 18:21:43 +0100

    create initialization script for creating symlinks in home

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+CONFIG_DIRECTORY=$(realpath "$(dirname "$0")")
+
+excluded_directories=(
+    ".git"
+)
+excluded_files=(
+    "init.sh"
+)
+
+exclude_directories=""
+for directory in "${excluded_directories[@]}"; do
+    exclude_directories+=" -type d -name '$directory' -prune -o "
+done
+
+exclude_files=""
+for file in "${excluded_files[@]}"; do
+    exclude_files+=" -not -name '$file' "
+done
+
+for file_path in $(eval "find '$CONFIG_DIRECTORY' $exclude_directories -type f $exclude_files -print"); do
+    file_relative_path=${file_path#"$CONFIG_DIRECTORY/"}
+    parent_relative_path=$(dirname "$file_relative_path")
+
+    mkdir -p "$HOME/$parent_relative_path"
+    ln -sf "$file_path" "$HOME/$file_relative_path"
+done
