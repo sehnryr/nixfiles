f7ef3208d263000f7919ccefdf88260ed7efa9c1
Youn Mélois <youn@melois.dev>
2025-05-15 21:18:15 +0200

    relaunch discord when closed for patching

--- .config/home-manager/programs/discord.nix
+++ .config/home-manager/programs/discord.nix
@@ -8,6 +8,8 @@
 let
   cfg = config.modules.discord;
 
+  discordPackage = cfg.package.override { withOpenASAR = cfg.useOpenASAR; };
+
   krisp-patcher =
     pkgs.writers.writePython3Bin "krisp-patcher"
       {
@@ -49,17 +51,26 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = [
-      (cfg.package.override { withOpenASAR = cfg.useOpenASAR; })
+      discordPackage
       (lib.mkIf cfg.useKrispPatcher krisp-patcher)
+      (lib.mkIf cfg.useKrispPatcher pkgs.procps)
     ];
 
     home.activation.krispPatcher = lib.mkIf cfg.useKrispPatcher (
       lib.hm.dag.entryAfter [ "installPackages" ] ''
-        run /usr/bin/pkill -f discord || true
+        discord_was_running=false
+        if ${pkgs.procps}/bin/pgrep -f discord > /dev/null; then
+          discord_was_running=true
+          run ${pkgs.procps}/bin/pkill -f discord || true
+        fi
 
         for node in "${config.xdg.configHome}/discord/"*"/modules/discord_krisp/discord_krisp.node"; do
           run ${krisp-patcher}/bin/krisp-patcher "$node"
         done
+
+        if [ "$discord_was_running" = true ]; then
+          run ${discordPackage}/bin/discord > /dev/null 2>&1 &
+        fi
       ''
     );
   };
