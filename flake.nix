7885f55dd9d3b3f841531b413133b61fc375c5ed
youn <youn@melois.dev>
2025-06-16 17:22:05 +0200

    feat(*): add clever-cloud config

--- flake.nix
+++ flake.nix
@@ -121,6 +121,19 @@
             nixos-hardware.nixosModules.framework-12th-gen-intel
           ];
         };
+        "clever-cloud" = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+            inherit user;
+            inherit fonts;
+          };
+
+          modules = [
+            ./nixos/clever-cloud
+            ./nixos/modules
+            nixos-hardware.nixosModules.lenovo-thinkpad-t480s
+          ];
+        };
       };
       # nix run home-manager/release-25.05 -- switch --flake .#<hostname>
       # home-manager switch --flake .#<hostname>
@@ -137,6 +150,22 @@
 
           modules = [
             ./home-manager/laptop.nix
+            ./home-manager/modules
+            agenix.homeManagerModules.default
+          ];
+        };
+        "${user.name}@clever-cloud" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          extraSpecialArgs = {
+            inherit inputs;
+            inherit user;
+            inherit ssh;
+            inherit fonts;
+          };
+
+          modules = [
+            ./home-manager/clever-cloud.nix
             ./home-manager/modules
             agenix.homeManagerModules.default
           ];
---
+++ home-manager/clever-cloud.nix
@@ -0,0 +1,93 @@
+{
+  pkgs,
+  lib,
+  user,
+  ssh,
+  ...
+}:
+
+{
+  home.username = user.name;
+  home.homeDirectory = user.homeDirectory;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  age = {
+    identityPaths = [ ssh.private.path ];
+    secrets = {
+      "arch-user-repository-ssh".file = ../secrets/arch-user-repository-ssh.age;
+      "clever-cloud-ssh".file = ../secrets/clever-cloud-ssh.age;
+    };
+  };
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "discord"
+      "slack"
+    ];
+
+  home.packages = with pkgs; [
+    # cli
+    tealdeer
+
+    # gui
+    nautilus # gnome file manager
+    evince # gnome document viewer
+    loupe # gnome image viewer
+    gnome-disk-utility
+    gnome-calculator
+    libreoffice-fresh
+    signal-desktop
+    thunderbird
+    resources
+    slack
+    vlc
+  ];
+
+  modules = {
+    # cli
+    ssh.enable = true;
+    git.enable = true;
+    direnv.enable = true;
+    neovim.enable = true;
+    nushell.enable = true;
+    sccache.enable = true;
+    carapace.enable = true;
+    starship.enable = true;
+
+    # gui
+    discord.enable = true;
+    ghostty.enable = true;
+    zen-browser.enable = true;
+    zed-editor.enable = true;
+
+    # desktop manager
+    gnome-shell = {
+      enable = true;
+      extensions = [
+        pkgs.gnomeExtensions.appindicator
+      ];
+      experimentalFeatures = [
+        "scale-monitor-framebuffer"
+        "xwayland-native-scaling"
+      ];
+      favoriteApps = [
+        "zen-twilight.desktop"
+        "dev.zed.Zed.desktop"
+        "com.mitchellh.ghostty.desktop"
+        "discord.desktop"
+      ];
+      showBatteryPercentage = false;
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
