fb5c0d1c6eb5ddfdcf64b6463ab297e045a36c4d
youn <youn@melois.dev>
2025-07-27 15:20:21 +0200

    add signal back

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -23,6 +23,7 @@
   };
 
   home.packages = with pkgs; [
+    signal-desktop
     beeper
     slack
   ];
--- home-manager/laptop.nix
+++ home-manager/laptop.nix
@@ -24,6 +24,7 @@
 
   home.packages = with pkgs; [
     signal-desktop
+    beeper
     slack
   ];
 
